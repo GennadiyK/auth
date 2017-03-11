e153f62f9e6d4a9c7a59924fbc0ae4f2e5aa67ff
hkalinouski <7742589@gmail.com>
2017-03-11 14:31:29 +0300

    added login and registr routes

--- index.js
+++ index.js
@@ -3,38 +3,42 @@ const app = new Koa();
 const bodyParser = require('koa-body-parser');
 const router = require('koa-router')();
 const User = require('./models/user');
+const passwordHash = require('password-hash');
 
 
 
 
 router.post('/registr', async (ctx) => {
   try {
-    
-    let user = await User.findOne({email: ctx.request.body.email});
-    
-    if( user === null){
       await User.create({
         email: ctx.request.body.email,
-        password: ctx.request.body.password
+        password:  passwordHash.generate(ctx.request.body.password)
       });
       ctx.body = 'Registered successfully';
-    } else {
-      ctx.body = 'Such user already exists';
-    }
+    
   } catch (err) {
+    if(err.code === 11000) {
+      ctx.throw(409, 'User already exists');
+    }
     throw(err);
   }
   
 });
 
-router.post('login', async (ctx) => {
+router.post('/login', async (ctx) => {
   try {
-   
-    console.log(user);
+    let user = await User.findOne({email: ctx.request.body.email});
+        
+    if(user && passwordHash.verify(ctx.request.body.password, user.password)) {
+      ctx.status = 200;
+      ctx.body = 'Loggined';
+    } else {
+      ctx.throw(401, 'You are not registered user');
+    }
   } catch (err) {
     throw(err);
   }
-  ctx.body = 'ok';
+ 
 });
 
 
